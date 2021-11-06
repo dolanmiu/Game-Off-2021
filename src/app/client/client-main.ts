2c867339b23553bc78cfd4b84e52d374f4172d4f
Dolan Miu <Dolan.miu@blackrock.com>
2021-11-06 03:54:34 +0000

    Add threejs

--- src/app/client/client-main.ts
+++ src/app/client/client-main.ts
@@ -3,6 +3,7 @@ import { Container, Inject, OnlyInstantiableByContainer } from 'typescript-ioc';
 import { ClientPlayerManager } from './player/client-player.manager';
 import { ClientNetworkManager } from './network/client-network.manager';
 import { ClientStoreManager } from './store/client-store.manager';
+import { runGame } from './game/main';
 
 @OnlyInstantiableByContainer
 class ClientMain {
@@ -12,6 +13,7 @@ class ClientMain {
       @Inject private readonly store: ClientStoreManager,
    ) {
       console.log('Client started.');
+      runGame();
    }
 }
 
