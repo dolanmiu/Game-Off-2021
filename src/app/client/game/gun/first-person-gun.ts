ee6c3db31a80cc2fa1af8161d1cc63462f742971
Dolan Miu <Dolan.miu@blackrock.com>
2021-11-15 00:27:22 +0000

    Add hand gun model to game

--- src/app/client/game/gun/first-person-gun.ts
+++ src/app/client/game/gun/first-person-gun.ts
@@ -1,10 +1,28 @@
 import { Audio, AudioListener, AudioLoader, Camera, Raycaster, Scene, Vector2 } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export class FirstPersonGun {
    private raycaster: Raycaster;
    private mousePosition: Vector2;
 
    public constructor(camera: Camera, scene: Scene) {
+      const loader = new GLTFLoader();
+
+      loader.load(
+         'assets/models/hand-gun.glb',
+         (model) => {
+            console.log(model);
+            model.scene.position.set(1, -0.5, -1.5);
+            model.scene.rotation.set(0, (Math.PI * 2) / 4 + Math.PI, 0);
+            model.scene.scale.set(2, 2, 2);
+            camera.add(model.scene);
+         },
+         undefined,
+         (error) => {
+            console.error(error);
+         },
+      );
+
       this.mousePosition = new Vector2();
       this.raycaster = new Raycaster();
       window.addEventListener(
