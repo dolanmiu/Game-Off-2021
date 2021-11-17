58b179f0fd5448ad5ffc05dcc74a4477d30011ef
Dolan Miu <dolan_miu@hotmail.com>
2021-11-17 02:58:43 +0000

    Add level generator

---
+++ src/app/client/game/level/level-generator.ts
@@ -0,0 +1,96 @@
+import {
+   BoxGeometry,
+   BufferGeometry,
+   Color,
+   Float32BufferAttribute,
+   Mesh,
+   MeshBasicMaterial,
+   MeshPhongMaterial,
+   Object3D,
+   PlaneGeometry,
+   Scene,
+   Vector3,
+} from 'three';
+
+import { LEVEL_META_DATA } from './level-meta-data';
+
+export class LevelGenerator {
+   private readonly vertex = new Vector3();
+   private readonly color = new Color();
+   private objects: THREE.Object3D[] = [];
+
+   public constructor(scene: Scene) {
+      LEVEL_META_DATA;
+
+      let floorGeometry: BufferGeometry = new PlaneGeometry(2000, 2000, 100, 100);
+      floorGeometry.rotateX(-Math.PI / 2);
+
+      // vertex displacement
+
+      let position = floorGeometry.attributes.position;
+
+      for (let i = 0, l = position.count; i < l; i++) {
+         this.vertex.fromBufferAttribute(position, i);
+
+         this.vertex.x += Math.random() * 20 - 10;
+         this.vertex.y += Math.random() * 2;
+         this.vertex.z += Math.random() * 20 - 10;
+
+         position.setXYZ(i, this.vertex.x, this.vertex.y, this.vertex.z);
+      }
+
+      floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+      position = floorGeometry.attributes.position;
+      const colorsFloor = [];
+
+      for (let i = 0, l = position.count; i < l; i++) {
+         this.color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+         colorsFloor.push(this.color.r, this.color.g, this.color.b);
+      }
+
+      floorGeometry.setAttribute('color', new Float32BufferAttribute(colorsFloor, 3));
+
+      const floorMaterial = new MeshBasicMaterial({ vertexColors: true });
+
+      const floor = new Mesh(floorGeometry, floorMaterial);
+      scene.add(floor);
+
+      // objects
+
+      const boxGeometry = new BoxGeometry(20, 20, 20).toNonIndexed();
+
+      position = boxGeometry.attributes.position;
+      const colorsBox = [];
+
+      for (let i = 0, l = position.count; i < l; i++) {
+         this.color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+         colorsBox.push(this.color.r, this.color.g, this.color.b);
+      }
+
+      boxGeometry.setAttribute('color', new Float32BufferAttribute(colorsBox, 3));
+
+      for (let i = 0; i < LEVEL_META_DATA.map.length; i++) {
+         for (let j = 0; j < LEVEL_META_DATA.map[i].length; j++) {
+            const cell = LEVEL_META_DATA.map[i][j];
+
+            switch (cell) {
+               case 1: {
+                  const boxMaterial = new MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
+                  boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+                  const box = new Mesh(boxGeometry, boxMaterial);
+                  box.position.x = i * 20;
+                  box.position.y = 10;
+                  box.position.z = j * 20;
+                  scene.add(box);
+                  this.objects.push(box);
+               }
+            }
+         }
+      }
+   }
+
+   public get Objects(): Object3D[] {
+      return this.objects;
+   }
+}
--- src/app/client/game/main.ts
+++ src/app/client/game/main.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 
 import { Controls } from './controls/controls';
 import { FirstPersonGun } from './gun/first-person-gun';
+import { LevelGenerator } from './level/level-generator';
 
 export const runGame = (): void => {
    let camera: THREE.PerspectiveCamera;
@@ -9,12 +10,9 @@ export const runGame = (): void => {
    let renderer: THREE.WebGLRenderer;
    let controls: Controls;
    let gun: FirstPersonGun;
-
-   const objects: THREE.Object3D[] = [];
+   let levelGenerator: LevelGenerator;
 
    let prevTime = performance.now();
-   const vertex = new THREE.Vector3();
-   const color = new THREE.Color();
 
    init();
    animate();
@@ -57,95 +55,28 @@ export const runGame = (): void => {
 
       scene.add(camera);
 
-      // floor
-
-      let floorGeometry: THREE.BufferGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
-      floorGeometry.rotateX(-Math.PI / 2);
-
-      // vertex displacement
-
-      let position = floorGeometry.attributes.position;
-
-      for (let i = 0, l = position.count; i < l; i++) {
-         vertex.fromBufferAttribute(position, i);
-
-         vertex.x += Math.random() * 20 - 10;
-         vertex.y += Math.random() * 2;
-         vertex.z += Math.random() * 20 - 10;
-
-         position.setXYZ(i, vertex.x, vertex.y, vertex.z);
-      }
-
-      floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-
-      position = floorGeometry.attributes.position;
-      const colorsFloor = [];
-
-      for (let i = 0, l = position.count; i < l; i++) {
-         color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-         colorsFloor.push(color.r, color.g, color.b);
-      }
-
-      floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
-
-      const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
-
-      const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-      scene.add(floor);
-
-      // objects
-
-      const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
-
-      position = boxGeometry.attributes.position;
-      const colorsBox = [];
-
-      for (let i = 0, l = position.count; i < l; i++) {
-         color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-         colorsBox.push(color.r, color.g, color.b);
-      }
-
-      boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
-
-      for (let i = 0; i < 500; i++) {
-         const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
-         boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-
-         const box = new THREE.Mesh(boxGeometry, boxMaterial);
-         box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
-         box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
-         box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
-
-         scene.add(box);
-         objects.push(box);
-      }
+      levelGenerator = new LevelGenerator(scene);
 
       gun = new FirstPersonGun(camera, scene);
-
-      //
 
       renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(window.innerWidth, window.innerHeight);
       document.body.appendChild(renderer.domElement);
 
-      //
+      window.addEventListener('resize', () => {
+         camera.aspect = window.innerWidth / window.innerHeight;
+         camera.updateProjectionMatrix();
 
-      window.addEventListener('resize', onWindowResize);
-   }
-
-   function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
+         renderer.setSize(window.innerWidth, window.innerHeight);
+      });
    }
 
    function animate() {
       requestAnimationFrame(animate);
       const time = performance.now();
       const delta = (time - prevTime) / 1000;
-      controls.update(delta, objects);
+      controls.update(delta, levelGenerator.Objects);
       gun.update(delta);
       prevTime = time;
       renderer.render(scene, camera);
